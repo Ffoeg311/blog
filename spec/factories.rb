10f6336406310a90c077470db30b4e4771391aba
Geoff Porter <geoff.porter@gmail.com>
2015-07-27 23:08:54 -0400

    Added specs for user model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,16 @@
 require 'faker'
+require 'devise'
 
 FactoryGirl.define do
   factory :article do |a|
     a.title { Faker::Lorem.sentence }
     a.content { Faker::Lorem.paragraph }
     a.user_id { Faker::Number.number(3) }
+  end
+
+  factory :user do |u|
+    u.email { Faker::Internet.email }
+    u.password { Faker::Internet.password(8) }
   end
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+ require 'spec_helper'
+
+ describe User do
+
+  it 'has a valid factory' do
+    FactoryGirl.create(:user).should be_valid
+  end
+
+  it 'is not accepted by default' do
+    puts FactoryGirl.create(:user).accepted.should be false
+  end
+
+  it 'is invalid without an email' do
+    FactoryGirl.build(:user, email: nil).should_not be_valid
+  end
+
+  it 'is invalid without a password' do
+    FactoryGirl.build(:user, password: nil).should_not be_valid
+  end
+end
+ 
