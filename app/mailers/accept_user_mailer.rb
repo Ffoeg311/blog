b3da14dd933871713b02f64f1a22737b6d8dc283
Geoff Porter <geoff.porter@gmail.com>
2015-07-29 20:59:53 -0400

    Renamed mailer to something more descriptive.

--- app/mailers/accept_user_mailer.rb
+++ app/mailers/accept_user_mailer.rb
@@ -1,8 +1,7 @@
-class UserAcceptanceMailer < ApplicationMailer
+class AcceptUserMailer < ApplicationMailer
   default from: 'notifications@example.com'
  
   def accept_user_request_email
-    @url  = 'http://example.com/login'
     mail(to: 'email_from_config@dummy.foo', subject: 'A new user requires approval')
   end
 end
--- spec/mailers/new_user_confirm_mailer_spec.rb
+++ spec/mailers/new_user_confirm_mailer_spec.rb
@@ -1,5 +1,7 @@
 require "rails_helper"
 
-RSpec.describe NewUserConfirmMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe AcceptUserMailer, type: :mailer do
+  it "has a valid factory" do
+    
+  end
 end
